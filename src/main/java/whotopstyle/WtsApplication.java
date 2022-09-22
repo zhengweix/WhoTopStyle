60257cb6dc4f5cce92753ff82e461d69923d402b
zhengweix <zheng.wei@famewell.com>
2022-09-22 02:29:08 -0700

    Added 220922

---
+++ src/main/java/whotopstyle/WtsApplication.java
@@ -0,0 +1,13 @@
+package whotopstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/whotopstyle/WtsApplicationTests.java
@@ -0,0 +1,13 @@
+package whotopstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
